680fe7808c669b2c15f7ddfe8919865592c8f93e
hvgupta <hvgupta@outlook.in>
2023-06-14 15:13:11 +0800

    hm

---
+++ SPG.py
@@ -0,0 +1,12 @@
+from game import *
+from Node import *
+
+class SPG():
+    def __init__(self) -> None:
+        initBoard = Board()
+        White_pList, Black_pList = initBoard.initialise(initBoard.board)
+        WhiteK, BlackK = getKing(White_pList, Black_pList)
+        self.game = Game(White_pList, Black_pList, WhiteK, BlackK, Colour.w.value, initBoard)
+        self.memory: list[Game,int,int] = []
+        self.root:Node = None
+        self.node:Node = None
